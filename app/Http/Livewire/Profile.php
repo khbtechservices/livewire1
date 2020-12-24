8c03a1be9cbfeb8df13eb187e027bc98d872aff4
Ken Burres <ken@khbtechservices.com>
2020-12-24 16:35:30 -0500

    TDDing A Profile Form - Done

--- app/Http/Livewire/Profile.php
+++ app/Http/Livewire/Profile.php
@@ -6,6 +6,29 @@ use Livewire\Component;
 
 class Profile extends Component
 {
+    public $username = '';
+
+    public $about = '';
+
+
+
+    public function save() {
+
+
+        $data = $this->validate([
+            'username' => 'required|alpha_num|min:6|max:30',
+            'about' => 'max:120'
+        ]);
+
+        auth()->user()->update($data);
+    }
+
+    // public function updateUsername() {
+    //     $this->validate([
+    //         'username' => 'alpha_num'
+    //     ]);
+    // }
+
     public function render()
     {
         return view('livewire.profile');
--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -6,6 +6,9 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use Illuminate\Support\Str;
+use Livewire\Livewire;
+
 use App\User;
 
 
@@ -32,7 +35,85 @@ class ProfileTest extends TestCase
 
         $user = factory(User::class)->create();
 
-        
+        Livewire::actingAs($user)
+            ->test('profile')
+            ->set('username', 'foobar')
+            ->set('about', 'bar')
+            ->call('save');
+
+        $user->refresh();
+
+        $this->assertEquals('foobar', $user->username);
+        $this->assertEquals('bar', $user->about);
+
+    }
+
+    /** @test **/
+    function username_is_required() {
+
+        $user = factory(User::class)->create();
+
+        Livewire::actingAs($user)
+            ->test('profile')
+            ->set('about', 'bar')
+            ->call('save')
+            ->assertHasErrors(['username' => 'required']);
+
+    }
+
+    /** @test **/
+    function username_meets_min_length_requirement() {
+
+        $user = factory(User::class)->create();
+
+        Livewire::actingAs($user)
+            ->test('profile')
+            ->set('username', Str::random(5))
+            ->set('about', 'bar')
+            ->call('save')
+            ->assertHasErrors(['username' => 'min']);
+
+    }
+
+    /** @test **/
+    function username_meets_max_length_requirement() {
+
+        $user = factory(User::class)->create();
+
+        Livewire::actingAs($user)
+            ->test('profile')
+            ->set('username', Str::random(31))
+            ->set('about', 'bar')
+            ->call('save')
+            ->assertHasErrors(['username' => 'max']);
+
+    }
+
+    /** @test **/
+    function username_meets_alphanum_requirement() {
+
+        $user = factory(User::class)->create();
+
+        Livewire::actingAs($user)
+            ->test('profile')
+            ->set('username', 'asdlj!!asdlkad')
+            ->set('about', 'bar')
+            ->call('save')
+            ->assertHasErrors(['username' => 'alpha_num']);
+
+    }
+
+    /** @test **/
+    function about_meets_max_length_requirement() {
+
+        $user = factory(User::class)->create();
+
+        Livewire::actingAs($user)
+            ->test('profile')
+            ->set('username', Str::random(10))
+            ->set('about', Str::random(150))
+            ->call('save')
+            ->assertHasErrors(['about' => 'max']);
 
     }
 
