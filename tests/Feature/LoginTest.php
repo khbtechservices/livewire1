5b8de79d10199fbda2007a512eef6e8986ce96b0
Ken Burres <ken@khb3.com>
2020-12-24 13:07:54 -0500

    Finished prepping for TDDing A Profile Form

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Route;
 use Tests\TestCase;
 
 use Livewire\Livewire;
@@ -46,8 +47,84 @@ class LoginTest extends TestCase
             auth()->user()->is(User::where('email', $user->email)->first())
         );
 
+    }
+
+    /** @test **/
+    function is_redirected_to_intended_after_login_prompt_from_auth_guard() {
+
+        Route::get('/intended')->middleware('auth'); //establish route
+
+        $user = factory(User::class)->create();
+
+        $this->get('/intended')->assertRedirect('/login'); //should get redirected to auth.login
+
+        Livewire::test('auth.login')
+            ->set('email', $user->email)
+            ->set('password', 'password')
+            ->call('login')
+            ->assertRedirect('/intended');
 
     }
 
+    /** @test */
+    public function is_redirected_to_root_after_login()
+    {
+        $user = factory(User::class)->create();
+
+        Livewire::test('auth.login')
+            ->set('email', $user->email)
+            ->set('password','password')
+            ->call('login')
+            ->assertRedirect('/');
+
+    }
+
+
+    /** @test */
+    public function email_is_required()
+    {
+
+        Livewire::test('auth.login')
+            ->call('login')
+            ->assertHasErrors(['email' => 'required']);
+    }
+
+    /** @test */
+    public function password_is_required()
+    {
+
+        $user = factory(User::class)->create();
+
+        Livewire::test('auth.login')
+            ->set('email', $user->email)
+            ->call('login')
+            ->assertHasErrors(['password' => 'required']);
+    }
+
+    /** @test */
+    public function email_must_be_valid_email()
+    {
+        // $user = factory(User::class)->create();
+
+        Livewire::test('auth.login')
+            ->set('email', 'testing.123')
+            ->call('login')
+            ->assertHasErrors(['email' => 'email']);
+
+    }
+
+    /** @test */
+    public function bad_login_attempt_shows_message()
+    {
+        $user = factory(User::class)->create();
+
+        Livewire::test('auth.login')
+            ->set('email',$user->email)
+            ->set('password','not-the-correct-password')
+            ->call('login')
+            ->assertHasErrors('email');
+
+        $this->assertNull(auth()->user());
+    }
 
 }
