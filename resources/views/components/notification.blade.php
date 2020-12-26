397ed388ed02e10ceddb60ade200d4858785289f
Ken Burres <ken@khbtechservices.com>
2020-12-26 07:38:11 -0500

    Toaster Notifications - Done

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -2,7 +2,7 @@
 
     <div
         x-data="{show:false, message:''}"
-        x-on:notify.window="show = true; message = $event.detail; "
+        x-on:notify.window="show = true; message = $event.detail; setTimeout( () => {show = false}, 2500 )"
         x-show="show"
         class="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto"
         x-transition:enter="transform ease-out duration-300 transition"
@@ -17,8 +17,8 @@
             <div class="p-4">
                 <div class="flex items-start">
                     <div class="flex-shrink-0">
-                        <svg class="h-6 w-6 text-green-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        <svg class="h-6 w-6 text-green-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                         </svg>
                     </div>
                     <div class="ml-3 w-0 flex-1 pt-0.5">
